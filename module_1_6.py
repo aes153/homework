4da821b653ca981927073a290c2dd399c0676234
aes153 <senaner07@gmail.com>
2024-10-11 18:21:54 +0300

    Update and rename module_1_6.py to module_1_4.py

--- module_1_6.py
+++
@@ -1,5 +0,0 @@
-number_of_homework = 12
-number_of_hours_spent = 1.5
-course_name = 'Python,'
-time_for_one_task = number_of_hours_spent/number_of_homework
-print('Курс:',course_name,' всего задач: ',number_of_homework,', затрачено часов: ',number_of_hours_spent,', среднее время выполнения ',time_for_one_task,'.',sep='')
