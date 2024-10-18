9dc94665e1fff5aa20f3b22cd2990a17bc434383
aes153 <senaner07@gmail.com>
2024-10-18 14:15:17 +0300

    Update Module_2_2.py

--- Module_2_2.py
+++ Module_2_2.py
@@ -1,21 +1,3 @@
-# name = input('Введите Ваше имя: ')
-# if name == 'Urban':
-#     print("Здравствуйте, администратор")
-# if name == "Анна":
-#     print('Здравствуй, ученик')
-# if name != 'Urban' and name != "Анна":
-#     print('Привет,', name)
-
-# number = int(input('Введите число: '))
-# if number % 3 == 0 and number % 5 == 0:
-#     print('FizzBuzz')
-# elif number % 3 == 0:
-#     print('Fizz')
-# elif number % 5 == 0:
-#     print('Buzz')
-# else:
-#     print('Число не подходит')
-
 first = int(input('Введите число: '))
 second = int(input('Введите число: '))
 third = int(input('Введите число: '))
