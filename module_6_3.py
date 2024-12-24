23fc94cb4df70cafadb8036665261cdf73e696c9
aes153 <senaner07@gmail.com>
2024-12-24 19:24:40 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,65 @@
+import random
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = speed
+
+    def move(self, dx, dy, dz):
+        if self._cords[2] + dz * self.speed < 0:
+            print('Это слишком глубоко, я не могу нырнуть:(')
+        else:
+            self._cords[0] += dx * self.speed
+            self._cords[1] += dy * self.speed
+            self._cords[2] += dz * self.speed
+
+    def get_cords(self):
+        print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print('Извини, я миролюбив :)')
+        else:
+            print('Будь осторожен, я нападаю на тебя 0_0')
+
+    def speak(self):
+        if self.sound is not None:
+            print(self.sound)
+
+class Bird(Animal):
+    beak = True
+
+    def lay_eggs(self):
+        n = random.randint(1, 4)
+        print(f'Вот {n} яйца для тебя')
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        self._cords[2] -= abs(dz) * (self.speed // 2)
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    sound = "Click-click-click"
+
+    def __init__(self, speed):
+        super().__init__(speed)
+
+# Пример использования
+db = Duckbill(10)
+print(db.live)
+print(db.beak)
+db.speak()
+db.attack()
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+db.lay_eggs()
