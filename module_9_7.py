c5054e38aeecae8c8c83d8d60bb5d57400496b5b
aes153 <senaner07@gmail.com>
2024-12-28 20:13:08 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,30 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+
+        def check_prime(n):
+            if n < 2:
+                return False
+            for i in range(2, int(n ** 0.5) + 1):
+                if n % i == 0:
+                    return False
+            return True
+
+        if check_prime(result):
+            print("Простое")
+        else:
+            print("Составное")
+
+        return result
+
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
+
+result = sum_three(5, 7, 13)
+print(result)
