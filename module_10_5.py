813097f8c5acb13369902548c33d50471c17263e
aes153 <senaner07@gmail.com>
2025-01-15 13:58:08 +0300

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,28 @@
+import time
+from multiprocessing import Pool
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            all_data.append(line.strip())
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_time = time.time() - start_time
+    print(f"Время выполнения линейного вызова: {linear_time} секунд")
+
+    # Многопроцессный вызов
+    start_time = time.time()
+    with Pool() as pool:
+        pool.map(read_info, filenames)
+    multiprocessing_time = time.time() - start_time
+    print(f"Время выполнения многопроцессного вызова: {multiprocessing_time} секунд")
